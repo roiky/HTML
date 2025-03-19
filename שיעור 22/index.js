456dda0304be3c6711dfba1f826ccd7d97aaac0a
roiky <iroei@hotmail.co.il>
2025-03-19 19:48:28 +0200

    class19032025

---
+++ שיעור 22/index.js
@@ -0,0 +1,19 @@
+
+let pageColor = 'white'
+
+function changeColor(){
+    if(pageColor === 'white'){
+        $("body").css("background-color", "black");
+        pageColor = 'black'
+    }
+    else if(pageColor==='black'){
+        $("body").css("background-color", "white");
+        pageColor = 'white'
+    }
+}
+
+const colorInterval = setInterval(changeColor, 5000);
+
+setTimeout(() => {
+    clearInterval(colorInterval)
+}, 20 * 1000);
