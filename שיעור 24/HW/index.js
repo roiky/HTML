dbfcb7690cfc35318f949ed95f2bc838a2bca050
roiky <iroei@hotmail.co.il>
2025-04-03 22:18:31 +0300

    lateHW

--- שיעור 24/HW/index.js
+++ שיעור 24/HW/index.js
@@ -1,6 +1,8 @@
 const DOM = {
     loader: null,
     countriesDDL: null,
+    sendButton: null,
+    countriesContent: null,
 };
 
 const BSIcons = {
@@ -17,8 +19,27 @@ const BSIcons = {
 function init() {
     DOM.loader = document.getElementById("loader"); //showLoader(DOM.loader);
     DOM.countriesDDL = document.getElementById("countriesSelect");
+    DOM.sendButton = document.getElementById("sendBtn");
+    DOM.countriesContent = document.getElementById("countryDetailsContent");
 
     loadCountriesToDDL();
+
+    DOM.sendButton.addEventListener("click", async function () {
+        console.log(DOM.countriesDDL.value);
+        try {
+            showLoader(DOM.loader, true);
+            const country = await getSpecificCountryByCode(DOM.countriesDDL.value);
+            if (country) {
+                console.log(country);
+                cleanContent("countryDetailsContent");
+                drawCountryFlag(country, "countryDetailsContent");
+            }
+        } catch (error) {
+            console.log(error);
+        } finally {
+            showLoader(DOM.loader, false);
+        }
+    });
 }
 
 init();
@@ -30,25 +51,60 @@ async function getCountriesFromAPI() {
 }
 
 async function loadCountriesToDDL() {
-    showLoader(DOM.loader);
+    showLoader(DOM.loader, true);
     try {
         const fetchResult = await getCountriesFromAPI();
         console.log(fetchResult, "drawing...");
+
         fetchResult.forEach((currentCountry) => {
-            const optionElement = `<option value='${currentCountry.cca3}'> ${currentCountry?.name?.common} </option>`;
-            DOM.countriesDDL.innerHTML += optionElement;
+            const optionElement = document.createElement("option");
+            optionElement.value = currentCountry.cca3;
+            optionElement.innerText = currentCountry?.name?.common;
+
+            DOM.countriesDDL.appendChild(optionElement);
         });
     } catch (error) {
         console.log(`something went wrong!`, error);
     } finally {
-        hideLoader(DOM.loader);
+        showLoader(DOM.loader, false);
     }
 }
 
-function showLoader(loaderElement) {
-    loaderElement.style.display = "";
+async function getSpecificCountryByCode(code) {
+    const result = await fetch(`https://restcountries.com/v3.1/alpha/${code}`);
+    const data = await result.json(); // await = take only the value.
+    const firstCountry = data[0];
+
+    const countryFlag = await fetch(firstCountry?.flags?.svg); //works but gives you the SVG text, if i want just the picture URL i dont need to "fetch" that!
+    const svgText = await countryFlag.text();
+
+    return {
+        name: firstCountry?.name?.common,
+        flag: firstCountry?.flags?.png,
+    };
 }
 
-function hideLoader(loaderElement) {
-    loaderElement.style.display = "none";
+function drawCountryFlag(item, whereToDraw) {
+    const targetElement = document.getElementById(whereToDraw);
+
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("d-inline-block", "w-auto", "p-2", "mt-2", "mr-2", "border");
+    newDiv.style.border = "1px solid black";
+    const countryName = document.createElement("h3");
+    countryName.innerText = item.name;
+
+    const countryFlag = document.createElement("img");
+    countryFlag.src = item.flag;
+
+    newDiv.append(countryName, countryFlag);
+    targetElement.appendChild(newDiv);
+}
+
+function cleanContent(whereToClean) {
+    const targetElement = document.getElementById(whereToClean);
+    targetElement.innerHTML = "";
+}
+
+function showLoader(loaderElement, display) {
+    display ? (loaderElement.style.display = "") : (loaderElement.style.display = "none");
 }
