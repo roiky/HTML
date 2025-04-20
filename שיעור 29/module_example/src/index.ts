d4e9f1b09c13642d786655829feadffbd6db5b8a
roiky <iroei@hotmail.co.il>
2025-04-20 22:30:29 +0300

    class20042025

---
+++ שיעור 29/module_example/src/index.ts
@@ -0,0 +1,35 @@
+console.log("my ts file?");
+
+const job: string = "Data Analyst"
+const address: string = "Rishon Lezion"
+const username: string = "roei"
+const lastname: string = "kalimi"
+const car: {
+    plateNum: number,
+    manufacture: string,
+    model: string,
+    year: number,
+    color: string
+} =
+{
+    plateNum: 123456789,
+    manufacture: "Hyundai",
+    model: "i10",
+    year: 2017,
+    color: "white"
+}
+
+const pet: {
+    name: string,
+    type: string,
+    age: number
+} =
+{
+    name: "Kiara",
+    type: "dog",
+    age: 2
+}
+
+//const failure: string = 32
+console.log("see the changes??")
+console.log("file created at dist folder?") 
