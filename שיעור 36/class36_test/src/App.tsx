d786950cb634043bf7b1e9de84408c406142e996
roiky <iroei@hotmail.co.il>
2025-06-08 21:03:01 +0300

    class08062005

---
+++ שיעור 36/class36_test/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import ShowMyName from "./components/name-app";
+import ShowMyPic from "./components/pic-app";
+
+function App() {
+    return (
+        <>
+            <ShowMyName name="Roei Kalimi" />
+            <ShowMyPic source="https://media.istockphoto.com/id/1372997793/vector/cute-pembroke-welsh-corgi-dog-waving-paw-vector-illustration.jpg?s=612x612&w=0&k=20&c=T_GXRG6RG5Oy07rHGrR6XvKDQGY9mjeCmxjJ_oIVTGM=" />
+        </>
+    );
+}
+
+export default App;
