22f5bab84d283d2a65b705f5e901af42baf1e47a
roiky <iroei@hotmail.co.il>
2025-06-14 19:01:14 +0300

    my-react-test

--- react-my-page/roei-page-test/src/components/Buttons/index.tsx
+++ react-my-page/roei-page-test/src/components/Buttons/index.tsx
@@ -8,13 +8,13 @@ export default function CreateButton(props: { text: string; img: string; url: st
     const [counter, setCounter] = useState<number>(0);
     return (
         <div className={css.buttonDiv}>
-            {/* <button className={css.customButton} onClick={handleClick}> */}
+            {/* <button className={css.customButton} onClick={handleClick}> */} {/*on cliuck send user to the URL*/}
             <button className={css.customButton} onClick={() => setCounter(counter + 1)}>
                 <img src={props.img} />
             </button>
-            <span style={{ fontWeight: "bold" }}>
-                {props.text} - {counter}
-            </span>{" "}
+            <span style={{ fontWeight: "bold", color: "black" }}>
+                {counter === 0 ? props.text : `${props.text} - ${counter}`}
+            </span>
         </div>
     );
 }
