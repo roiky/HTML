a12d7766167e0bb6ef27076316c33d49894f4faa
roiky <iroei@hotmail.co.il>
2025-07-11 10:48:55 +0300

    hw09072025

--- שיעור 43/gov-il-client/src/components/pages/factoriesPage/index.tsx
+++ שיעור 43/gov-il-client/src/components/pages/factoriesPage/index.tsx
@@ -3,50 +3,81 @@ import { Box, FormControl, InputLabel, MenuItem, Select } from "@mui/material";
 import { useEffect, useState } from "react";
 import { getFactoriesApi, type FactoryClient } from "./service/getFactoriesApi";
 import { PieChart } from "@mui/x-charts/PieChart";
-import { getFactoriesReport } from "./utils";
+import { getFactoriesByKey } from "./utils";
 
 export default function FactoryPage() {
     const [isLoading, setIsLoading] = useState(false);
     const [factories, setFactories] = useState<Array<Partial<FactoryClient>>>([]);
+    const [limit, setLimit] = useState("10");
+
+    const limitOptions = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 
     useEffect(() => {
         async function getFactories() {
             try {
                 setIsLoading(true);
-                const result = await getFactoriesApi();
+                const result = await getFactoriesApi(limit);
                 setFactories(result);
+                console.log(result);
             } catch (error) {
             } finally {
                 setIsLoading(false);
             }
         }
         getFactories();
-    }, []);
+    }, [limit]);
 
-    const factoriesPerLocationReport = getFactoriesReport(factories);
+    const factoriesPerLocationReport = getFactoriesByKey(factories, "localAuthority");
+    const factoriesPerMachozReport = getFactoriesByKey(factories, "district");
 
-    const handleChange = () => {
-        console.log("changed");
+    const handleChange = (event: any) => {
+        setLimit(event.target.value as string);
     };
+
     return (
         <div style={{ margin: "auto", width: "80%" }}>
             <h1> Factory Page</h1>
             <div>
                 <FormControl fullWidth>
-                    <InputLabel id="demo-simple-select-label">Age</InputLabel>
-                    <Select labelId="demo-simple-select-label" id="demo-simple-select" value={10} label="Age" onChange={handleChange}>
-                        <MenuItem value={10}>10</MenuItem>
+                    <InputLabel id="demo-simple-select-label">Records to get</InputLabel>
+                    <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        value={limit}
+                        label="Age"
+                        onChange={handleChange}
+                    >
+                        {/* <MenuItem value={10}>10</MenuItem>
                         <MenuItem value={20}>20</MenuItem>
-                        <MenuItem value={30}>30</MenuItem>
+                        <MenuItem value={30}>30</MenuItem> */}
+
+                        {limitOptions.map((l) => (
+                            <MenuItem key={l} value={l}>
+                                {l}
+                            </MenuItem>
+                        ))}
                     </Select>
                 </FormControl>
             </div>
             <div>
+                <h3 style={{ textAlign: "center" }}>Facories Per Local Authority</h3>
                 <Box component="form" sx={{ "& > :not(style)": { m: 1 } }} noValidate autoComplete="off">
                     <PieChart
                         series={[
                             {
                                 data: factoriesPerLocationReport,
+                            },
+                        ]}
+                        width={400}
+                        height={400}
+                    />
+                </Box>
+                <h3 style={{ textAlign: "center" }}>Facories Per Machoz</h3>
+                <Box component="form" sx={{ "& > :not(style)": { m: 1 } }} noValidate autoComplete="off">
+                    <PieChart
+                        series={[
+                            {
+                                data: factoriesPerMachozReport,
                             },
                         ]}
                         width={400}
--- שיעור 43/gov-il-client/src/components/pages/factoriesPage/utils.ts
+++ שיעור 43/gov-il-client/src/components/pages/factoriesPage/utils.ts
@@ -1,22 +1,21 @@
 import type { FactoryClient } from "./service/getFactoriesApi";
 
-type PieChartData = Array<{ id: number, label: string, value: number }>
+type PieChartData = Array<{ id: number; label: string; value: number }>;
 
-export function getFactoriesReport(factories: Array<Partial<FactoryClient>>): PieChartData {
+export function getFactoriesByKey(factories: Array<Partial<FactoryClient>>, key: keyof FactoryClient): PieChartData {
     const result = factories.reduce((acc, currentFactory) => {
-        if (!currentFactory.localAuthority) return acc;
-        if (acc[currentFactory.localAuthority]) {
-            acc[currentFactory.localAuthority] += 1;
+        const groupKey = currentFactory[key];
+        if (!key || !groupKey) return acc;
+        if (acc[groupKey as string]) {
+            acc[groupKey as string] += 1;
         } else {
-            acc[currentFactory.localAuthority] = 1
+            acc[groupKey as string] = 1;
         }
 
         return acc;
-    }, {} as { [key: string]: number })
+    }, {} as { [key: string]: number });
 
     return Object.entries(result).map(([key, value], index) => {
-        return { id: index, value, label: key }
-    })
-
+        return { id: index, value, label: key };
+    });
 }
-
