bb4bdab27f44b8939502aa273fcd4e229e018394
roiky <iroei@hotmail.co.il>
2025-07-21 19:03:09 +0300

    hw20072025

---
+++ שיעור 45/Lesson_1/HW/index.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+const PORT = 3000;
+app.use(cors());
+app.use(bodyParser.json());
+
+const users = [];
+
+app.use((req, res, next) => {
+    console.log(`[${new Date().toISOString()}]: ${req.baseUrl}`);
+    next();
+});
+
+app.get("/", (req, res) => {
+    res.send("Welcome to my API!");
+});
+
+app.post("/register", (req, res, next) => {
+    //const { userName, password } = req.body;
+    const userName = req.body.userName;
+    const password = req.body.password;
+    if (!userName || !password) {
+        return next(new Error("Missing"));
+    }
+
+    const userExists = users.find((user) => user.userName === userName);
+    if (userExists) {
+        return next(new Error("userExist"));
+    }
+
+    users.push({ userName, password });
+    res.status(201).json({ message: "User registered successfully" });
+});
+
+app.post("/login", (req, res, next) => {
+    const userName = req.body.userName;
+    const password = req.body.password;
+    if (!userName || !password) {
+        return next(new Error("Missing"));
+    }
+
+    const validUser = users.find((user) => user.userName === userName && user.password === password);
+    if (validUser) {
+        res.status(200).json({ message: `${userName} logged in successfully!` });
+    } else {
+        return next(new Error("failedLogin"));
+    }
+});
+
+app.listen(PORT, (err) => {
+    if (err) {
+        console.log(err.message);
+    } else {
+        console.log(`API is running on port: ${PORT}`);
+    }
+});
+
+//error middleware
+app.use((error, req, res, next) => {
+    switch (error.message) {
+        case "Missing": {
+            return res.status(401).json({ message: "Missing username or password!" });
+        }
+        case "userExist": {
+            return res.status(401).json({ message: "User already exist!" });
+        }
+        case "failedLogin": {
+            return res.status(401).json({ message: "Wrong username or password!" });
+        }
+        default: {
+            return res.status(500).json({ message: "Something went wrong!" });
+        }
+    }
+});
