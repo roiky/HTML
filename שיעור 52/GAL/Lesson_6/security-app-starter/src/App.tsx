d478e00bcb59e031ec6d7917ca17d9c8e4e5c2ba
roiky <iroei@hotmail.co.il>
2025-08-21 19:56:55 +0300

    HW20082025

--- שיעור 52/GAL/Lesson_6/security-app-starter/src/App.tsx
+++ שיעור 52/GAL/Lesson_6/security-app-starter/src/App.tsx
@@ -1,30 +1,38 @@
-import { Routes, Route, Navigate } from 'react-router-dom'
-import Header from './components/Header'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import Data from './pages/Data'
-import ProtectedRoute from './components/ProtectedRoute'
+import { Routes, Route, Navigate } from "react-router-dom";
+import Header from "./components/Header";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Data from "./pages/Expenses";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Expenses from "./pages/Expenses";
 
 export default function App() {
-  return (
-    <div className="app">
-      <Header />
-      <main className="container">
-        <Routes>
-          <Route path="/" element={<Navigate to="/data" replace />} />
-          <Route
-            path="/data"
-            element={
-              <ProtectedRoute>
-                <Data />
-              </ProtectedRoute>
-            }
-          />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="*" element={<div className="card"><h2>Not Found</h2></div>} />
-        </Routes>
-      </main>
-    </div>
-  )
+    return (
+        <div className="app">
+            <Header />
+            <main className="container">
+                <Routes>
+                    <Route path="/" element={<Navigate to="/expenses" replace />} />
+                    <Route
+                        path="/expenses"
+                        element={
+                            <ProtectedRoute>
+                                <Expenses />
+                            </ProtectedRoute>
+                        }
+                    />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route
+                        path="*"
+                        element={
+                            <div className="card">
+                                <h2>Not Found</h2>
+                            </div>
+                        }
+                    />
+                </Routes>
+            </main>
+        </div>
+    );
 }
