9daa7bd3b6eba6d117f00c6fb1413961d5a64467
roiky <iroei@hotmail.co.il>
2025-09-29 20:13:29 +0300

    vacations_page_route

--- Project3_Vacations/client/src/App.tsx
+++ Project3_Vacations/client/src/App.tsx
@@ -6,6 +6,7 @@ import Lecturers from "./pages/Lecturers";
 import LoginPage from "./pages/Login";
 import RegisterPage from "./pages/Register";
 import Display404 from "./components/404";
+import VacationsPage from "./pages/Vacations";
 
 export default function App() {
     return (
@@ -14,9 +15,10 @@ export default function App() {
             <main className="container">
                 <Routes>
                     <Route path="/" element={<Navigate to="/lecturers" replace />} />
-                    <Route path="/lecturers" element={<Lecturers />} />
+                    {/* <Route path="/lecturers" element={<Lecturers />} /> */}
                     <Route path="/login" element={<LoginPage />} />
                     <Route path="/register" element={<RegisterPage />} />
+                    <Route path="/vacations" element={<VacationsPage />} />
                     <Route path="*" element={<Display404 />} />
                 </Routes>
             </main>
---
+++ Project3_Vacations/client/src/pages/Vacations.tsx
@@ -0,0 +1,3 @@
+export default function VacationsPage() {
+    return "VACATIONS";
+}
